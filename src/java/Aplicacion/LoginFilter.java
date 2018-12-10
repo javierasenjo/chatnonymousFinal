9f928f12601f2e4ba2f4e07e40c07710f5053db6
javie <javie@MSI-Javiers>
2018-12-10 18:52:37 +0100

    easteregg

--- src/java/Aplicacion/LoginFilter.java
+++ src/java/Aplicacion/LoginFilter.java
@@ -9,39 +9,48 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  *
  * @author ebenz
  */
+@WebFilter(filterName = "LoginFilter", urlPatterns = {"/ChatDisplay"})
+
 public class LoginFilter implements Filter {
-     protected FilterConfig filterConfig;
-     // Se llama cuando se crea una instancia del filtro. 
-     // Si se correlaciona con j_security_check, se llama 
-     // la primera vez que se invoca j_security_check.
-     @Override
-     public void init(FilterConfig filterConfig) throws ServletException {
-        this.filterConfig = filterConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("[Filtro login] Procesando Petición");
+        try {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            String user = (String) httpRequest.getSession().getAttribute("usuario");
+            if (user.equals("developer@ceu.es")) {
+                System.out.println("[Filtro login] Redirigiendo a easter egg");
+                ServletContext servletContext = httpRequest.getSession().getServletContext();
+                RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/easterEgg.xhtml");
+                requestDispatcher.forward(request, response);
+
+            } else {
+                //Redirigir
+                chain.doFilter(request, response);
+            }
+        } catch (Exception ex) {
+            throw ex;
         }
-     @Override
-     public void destroy() {
-        this.filterConfig = null;
-        }
-      // Se llama por cada solicitud correlacionada a este filtro. 
-     // Si se correlaciona con j_security_check, 
-     // se llama para cada acción j_security_check
-     @Override
-     public void doFilter(ServletRequest request, 
-     ServletResponse response, FilterChain chain) 
-         throws java.io.IOException, ServletException   {
-         // efectuar aquí la acción previa al inicio de sesión
-         chain.doFilter(request, response);  
-         // llama al siguiente filtro de la cadena.  
-         // j_security_check si este filtro 
-         // se correlaciona con j_security_check.
-        // efectuar aquí la acción posterior al inicio de sesión.
-                 }
-       }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
